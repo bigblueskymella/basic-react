47fc45c2a81c4d6c42c4e4a95015214de9727a89
bigblueskymella <110326250+bigblueskymella@users.noreply.github.com>
2024-12-02 19:54:47 +0900

    Update Header.js

--- react-blue-bottle/Header.js
+++ react-blue-bottle/Header.js
@@ -1,1 +1,24 @@
 
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <a href="index.html">
+          <img src="images/logo.png" alt="blue logo" width="60" />
+        </a>
+      </div>
+      <div className="nav">
+        <a href="about.html">ABOUT</a>
+        <a href="cafes.html">CAFES</a>
+        <a href="#">MENU</a>
+        <a href="#">COMPANY</a>
+        <a href="#">NEWS</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
