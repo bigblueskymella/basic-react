032cdc2f8e1876b7640dad8595adaefe2462639d
bigblueskymella <110326250+bigblueskymella@users.noreply.github.com>
2024-12-02 19:55:21 +0900

    Create Footer.js

---
+++ react-blue-bottle/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      CONNECT WITH US <br />
+      <br />
+      <a href="#">
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+      <a href="#">
+        <i className="fa-brands fa-facebook"></i>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
