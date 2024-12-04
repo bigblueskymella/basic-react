c3ba98f12bc9e7d622cd2a9625da0f98725f0328
bigblueskymella <110326250+bigblueskymella@users.noreply.github.com>
2024-12-04 10:43:43 +0900

    Update 1️⃣-1 Main.js

--- 1️⃣-1 Main.js
+++ 1️⃣-1 Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import './Main.css';
 
  const Main = (props) => { //💌1
+ //const Main = ({name,location}) => { //💌2
    console.log(props) 
   
   return (
     <div className="location">
       <h1>Main</h1>
       {props.name}는 {props.location}에 있습니다.
+       {/* {name}는 {location}에 있습니다. */}
     </div>
   );
 };
